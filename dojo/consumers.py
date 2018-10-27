17a04b69f08f7c02ee4e9bd2c9e4297e87cb145e
Ian Adam Naval <ian@everquote.com>
2018-10-26 23:25:40 -0400

    Extend live editing to both editors
    Hacky and buggy, but a good start.

--- dojo/consumers.py
+++ dojo/consumers.py
@@ -24,6 +24,7 @@ class SessionConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        editor = text_data_json['editor']
         message = text_data_json['message']
 
         # Send message to room group
@@ -31,15 +32,18 @@ class SessionConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
+                'editor': editor,
                 'message': message
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
+        editor = event['editor']
         message = event['message']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'editor': editor,
             'message': message
         }))
