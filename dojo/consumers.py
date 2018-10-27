07c05027d4ac42fd168509bf909664d9eeb34ad2
Ian Adam Naval <ian@everquote.com>
2018-10-26 23:07:35 -0400

    Add live code editing for tests

--- dojo/consumers.py
+++ dojo/consumers.py
@@ -1,10 +1,10 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class SessionConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.session_id = self.scope['url_route']['kwargs']['session_id']
+        self.room_group_name = 'chat_%s' % self.session_id
 
         # Join room group
         await self.channel_layer.group_add(
--- dojo/routing.py
+++ dojo/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<room_name>/', consumers.ChatConsumer),
+    path('ws/session/<session_id>/', consumers.SessionConsumer),
 ]
